3e5173732ddc740be96be533ce2cc827fa64e442
tygoee <87303800+tygoee@users.noreply.github.com>
2023-10-08 16:06:42 +0200

    Changed document structure for expansion

--- README.md
+++ README.md
@@ -1,13 +1,32 @@
-# papt
+# Papt - Personal apt
 
-Personal apt: installs apt packages locally in `~/.local/`, without needing root privileges. This script installs the packages in a temp directory using `dpkg -x` and moves them to `~/.local`.
+Personal apt: installs apt packages locally in `~/.local/`, without needing root privileges. This script installs the packages in a temp directory using `dpkg -x` and moves them to `~/.local/`.
 
----
+## Installation
 
-Usage: `./papt.sh <package-names|args>`
+First, clone the repository and cd there:
 
-Arguments: <br>
-`--setup`: Setups neccesarry files for installing and running these files <br>
+    git clone https://github.com/tygoee/papt
+    cd papt
+
+If you want this to be available as a command, copy it to `~/.local/bin/`:
+
+    mkdir -p ~/.local/bin/
+    cp ./papt.sh ~/.local/bin/papt
+
+Finally, setup papt:
+
+    papt --setup
+
+## Usage
+
+Command usage: `papt [options|args] command`
+
+**Options:** <br>
+`install`: Installs specified packages
+
+**Arguments:** <br>
+`--setup`: Sets up necessary files for installing and running these packages <br>
 `--help`: Display a help message
 
 ---
--- papt.sh
+++ papt.sh
@@ -1,109 +1,160 @@
 #!/bin/bash
 
+_help() {
+    # Display a help message
+    echo -e "\033[1mPapt v0.0 - Personal apt\033[0m"
+    echo "Usage: $0 [options|args] command"
+    echo
+    echo -e "\033[1mOptions:\033[0m"
+    echo "  install: Installs specified packages"
+    echo
+    echo -e "\033[1mArguments:\033[0m"
+    echo "  --setup: Sets up necessary for installing"\
+            "and running these packages"
+    echo "  --help:  Display this help message"
+}
+
+
+_install() {
+    # Install a package (and all dependencies)
+    if [ $# -eq 0 ]; then
+        echo "No packages specified."
+        exit 1
+    fi
+
+    # Make a temp dir and cd there
+    temp_dir="$HOME/.temp/"
+    mkdir -p "$temp_dir"
+    trap '[ -d "$HOME/.temp/" ] && rm -rf "$HOME/.temp/"' EXIT
+
+    cd "$temp_dir" || {
+        echo "Cannot CD into $temp_dir, exiting..."
+        exit 1;
+    }
+
+    # Exit if there's no internet connection
+    if ! ping -c 1 -W 1 "deb.debian.org" > /dev/null 2>&1; then
+        echo "You need an active internet connection"
+        exit 1
+    fi
+
+    # TODO: Check if all packages exist
+    # and give a confirmation prompt
+
+    # Download package and all dependencies
+    for package_name in "$@"; do
+        apt download "$package_name" || {
+            echo "Error while trying to install package, exiting...";
+            exit 1
+        }
+
+        echo "Collecting dependencies..."
+        dependencies=$(apt-cache depends --recurse -i "$package_name" |
+                        grep "Depends:" | awk '{print $2}' | sort -u)
+
+        for dependency in $dependencies; do
+            # TODO: Ignore virtual packages using
+            # apt download .. || { apt show | grep ..; }
+            apt download "$dependency"
+        done
+    done
+
+    # Install all of them in .temp/install/
+    for deb_file in "$temp_dir"*.deb; do
+        echo "Installing ${deb_file##*/}..."
+        dpkg -x "$deb_file" "install/"
+    done
+
+    # Cd into .temp/install/ and copy all files
+    # to their corresponding directories
+    echo "Moving files..."
+
+    inst_dir="$temp_dir/install"
+    local_dir="$HOME/.local/"
+    mkdir -p "$local_dir"
+
+    find "$inst_dir" -mindepth 1 -maxdepth 1 ! -name 'usr' -exec cp -rn {} "$local_dir" \;
+
+    if [ -d "$inst_dir/usr/" ]; then
+        find "$inst_dir/usr/" -mindepth 1 -maxdepth 1 ! -name 'local' -exec cp -rn {} "$local_dir" \;
+    fi
+
+    if [ -d "$inst_dir/usr/local/" ]; then
+        cp -rn "$inst_dir/usr/local/"* "$local_dir"
+    fi
+
+    # Echo Done when the
+    # installation is done
+    echo "Done."
+}
+
+
+_setup() {
+    # Setup all necessary files
+
+    # Create all needed dirs if they dont exist
+    mkdir -p "$HOME/.local/bin/"
+    mkdir -p "$HOME/.local/lib/"
+    mkdir -p "$HOME/.local/lib64/"
+    mkdir -p "$HOME/.local/lib/x86_64-linux-gnu/"
+
+    echo "Created library directories..."
+
+    # Add the library path to .profile
+    # if --setup is specified
+    lib_paths="$(printf '%s' \
+        "export LD_LIBRARY_PATH=" \
+        "$HOME/.local/lib:" \
+        "$HOME/.local/lib64:" \
+        "$HOME/.local/lib/x86_64-linux-gnu")"
+
+    # Append to $HOME/.profile
+    # if it isn't already there
+    if ! grep -q "$lib_paths" \
+            "$HOME/.profile"; then
+        echo "Adding library paths..."
+        echo "$lib_paths" >> "$HOME/.profile"
+        eval "$lib_paths"
+        echo "Please reboot your computer or"\
+                "login again to complete the setup"
+    else
+        echo "Library paths already"\
+                "present, ignoring..."
+    fi
+
+    echo "Done."
+}
+
+
+_syntaxerr() {
+    echo "Usage: $0 [options|args] command"
+    echo "See --help for more info"
+    exit 1
+}
+
+
 # Exit if there are no arguments
 if [ $# -eq 0 ]; then
-    echo "Usage: $0 <package-names|args>"
-    exit 1
+    _syntaxerr
 fi
 
-package_names=()
+# Execute the right command
+case "$1" in
+  # Arguments
+  --help)
+    _help
+    ;;
+  --setup)
+    _setup
+    ;;
 
-# Cycle through args
-for arg in "$@"; do
-    if [ "$arg" = "--help" ]; then
-        echo "General usage: $0 <package-names|args>"
-        exit 0
-    elif [ "$arg" = "--setup" ]; then
-        # Create all needed dirs if they dont exist
-        mkdir -p "$HOME/.local/lib/"
-        mkdir -p "$HOME/.local/lib64/"
-        mkdir -p "$HOME/.local/lib/x86_64-linux-gnu/"
+  # Options
+  install)
+    _install "${@:2}"
+    ;;
 
-        echo "Created library directories..."
-
-        # Add the library path to .profile
-        # if --setup is specified
-        lib_paths="$(printf '%s' \
-            "export LD_LIBRARY_PATH=" \
-            "$HOME/.local/lib:" \
-            "$HOME/.local/lib64:" \
-            "$HOME/.local/lib/x86_64-linux-gnu")"
-
-        # Append to $HOME/.profile
-        # if it isn't already there
-        if ! grep -q "$lib_paths" \
-                "$HOME/.profile"; then
-            echo "Adding library paths..."
-            echo "$lib_paths" >> "$HOME/.profile"
-            eval "$lib_paths"
-            echo "Please reboot your computer or"\
-                 "login again to complete the setup"
-        else
-            echo "Library paths already"\
-                 "present, ignoring..."
-        fi
-
-        echo "Done."
-        exit 0
-    else
-        package_names+=("$arg")
-    fi
-done
-
-# Make a temp dir and cd there
-temp_dir="$HOME/.temp/"
-
-mkdir -p "$temp_dir"
-cd "$temp_dir" || { echo "Cannot CD into $temp_dir, exiting..." && rm -r "$temp_dir" && exit 1; }
-
-# Exit if there's no internet connection
-if ! ping -c 1 -W 1 "deb.debian.org" > /dev/null 2>&1; then
-    echo "You need an active internet connection"
-    rm -r "$temp_dir"
-    exit 1
-fi
-
-# Download package and all dependencies
-for package_name in "${package_names[@]}"; do
-    apt download "$package_name" || { echo "Error while trying to install package, exiting..." && rm -r "$temp_dir" && exit 1; }
-
-    echo "Collecting dependencies..."
-    dependencies=$(apt-cache depends --recurse -i "$package_name" | grep "Depends:" | awk '{print $2}' | sort -u)
-
-    for dependency in $dependencies; do
-        # TODO: Ignore virtual packages using
-        # apt download .. || { apt show | grep ..; }
-        apt download "$dependency"
-    done
-done
-
-# Install all of them in .temp/install/
-for deb_file in "$temp_dir"*.deb; do
-    echo "Installing ${deb_file##*/}..."
-    dpkg -x "$deb_file" "install/"
-done
-
-# Cd into .temp/install/ and copy all files
-# to their corresponding directories
-echo "Moving files..."
-
-inst_dir="$temp_dir/install"
-local_dir="$HOME/.local/"
-mkdir -p "$local_dir"
-
-find "$inst_dir" -mindepth 1 -maxdepth 1 ! -name 'usr' -exec cp -rn {} "$local_dir" \;
-
-if [ -d "$inst_dir/usr/" ]; then
-    find "$inst_dir/usr/" -mindepth 1 -maxdepth 1 ! -name 'local' -exec cp -rn {} "$local_dir" \;
-fi
-
-if [ -d "$inst_dir/usr/local/" ]; then
-    cp -rn "$inst_dir/usr/local/"* "$local_dir"
-fi
-
-# Echo "Done." when the
-# installation is done
-# and remove the temp dir
-echo "Done."
-
-rm -r "$temp_dir"
+  # None of these
+  *)
+    _syntaxerr
+    ;;
+esac
